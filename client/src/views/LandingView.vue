18142f1d2d1e21f1cb57951e0162f9d9b07ea3b2
JesusKris <robertkrislaur.media@gmail.com>
2023-12-28 17:30:48 +0200

    Fixing whitespaces on the edges on mobile portrait mode

--- client/src/views/LandingView.vue
+++ client/src/views/LandingView.vue
@@ -3,8 +3,8 @@ import ScorePalLogoFull from '../components/LogoScorePalFull.vue'
 </script>
 
 <template>
-  <div class="carcassonne-castle-img w-full h-screen flex mobile-safe-padding flex-col flex-grow">
-    <div class="px-[7%] flex flex-1 flex-col items-center align-top">
+  <div class="carcassonne-castle-img w-screen h-screen flex mobile-safe-padding flex-col flex-grow">
+    <div class="px-[7%] flex flex-1 flex-col items-center align-top w-full">
       <ScorePalLogoFull />
     </div>
     <div class="flex-1 flex flex-col justify-center items-center w-full">
