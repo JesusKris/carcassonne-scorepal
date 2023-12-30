cdf884a578d52e4332258aa6958e78c112152980
JesusKris <robertkrislaur.media@gmail.com>
2023-12-30 14:28:32 +0200

    Optimizing responsivness

--- client/src/views/MainMenuView.vue
+++ client/src/views/MainMenuView.vue
@@ -5,16 +5,16 @@ import BlueButton from "../components/BlueButton.vue"
 </script>
 
 <template>
-  <div class="carcassonne-castle-img w-screen h-screen flex mobile-safe-padding flex-col flex-grow">
-    <div class="px-[7%] flex flex-1 flex-col items-center align-top">
+  <div class="carcassonne-castle-img h-screen flex flex-col">
+    <div class="px-[7%] flex flex-1 flex-col items-center justify-center">
       <ScorePalLogoFull />
     </div>
-    <div class="flex-1 flex flex-col items-center align-top py-5 box-border">
+    <!-- <div class="flex-1 flex flex-col items-center align-top py-5 box-border">
       <BlueButton :sprite-selector="'continue'" :is-shown="true" :callback="() => console.log(`hi`)" />
       <BlueButton :sprite-selector="'new-game'" :is-shown="true" :callback="() => console.log(`hi`)" />
       <BlueButton :sprite-selector="'history'" :is-shown="true" :callback="() => console.log(`hi`)" />
       <BlueButton :sprite-selector="'rules'" :is-shown="true" :callback="() =>console.log(`hi`)" />
-    </div>
+    </div> -->
     <MadeByJesusKris />
   </div>
 </template> 
