bd0732baadd068cd7543e7511f52c8e9afa5d753
JesusKris <robertkrislaur.media@gmail.com>
2023-12-30 14:23:51 +0200

    Optimizing responsivness

--- client/src/views/MainMenuView.vue
+++ client/src/views/MainMenuView.vue
@@ -6,16 +6,16 @@ import BlueButton from "../components/BlueButton.vue"
 
 <template>
   <div class="carcassonne-castle-img w-screen h-screen flex mobile-safe-padding flex-col flex-grow">
-   <!--  <div class="px-[7%] flex flex-1 flex-col items-center align-top w-full">
+    <div class="px-[7%] flex flex-1 flex-col items-center align-top">
       <ScorePalLogoFull />
-    </div> -->
+    </div>
     <div class="flex-1 flex flex-col items-center align-top py-5 box-border">
-<!--       <BlueButton :sprite-selector="'continue'" :is-shown="true" :callback="() => console.log(`hi`)" />
+      <BlueButton :sprite-selector="'continue'" :is-shown="true" :callback="() => console.log(`hi`)" />
       <BlueButton :sprite-selector="'new-game'" :is-shown="true" :callback="() => console.log(`hi`)" />
       <BlueButton :sprite-selector="'history'" :is-shown="true" :callback="() => console.log(`hi`)" />
-      <BlueButton :sprite-selector="'rules'" :is-shown="true" :callback="() =>console.log(`hi`)" /> -->
+      <BlueButton :sprite-selector="'rules'" :is-shown="true" :callback="() =>console.log(`hi`)" />
     </div>
-    <!-- <MadeByJesusKris /> -->
+    <MadeByJesusKris />
   </div>
 </template> 
 
